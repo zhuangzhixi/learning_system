a9947fe1495e199cc312ebdd7d75f4ba9768ab18
zhuangzhixi <2018418308@qq.com>
2023-04-02 20:53:53 +0800

    完成了tabbar功能

---
+++ pages/class/class.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    Class
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
